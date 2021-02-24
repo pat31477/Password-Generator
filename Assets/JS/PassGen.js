2752ad3d0b01479bde876f89cb2abe1daddf029a
Patrick Walker <pat31477@yahoo.com>
2021-02-24 10:58:16 -0500

    First draft of JS, defining varliables

---
+++ Assets/JS/PassGen.js
@@ -0,0 +1,17 @@
+
+//define variables
+
+var lower = 'abcdefghijklmnopqrstuvwxyz';
+var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var special = '!@#$^&%*()+=-[]{}|:<>?,.';
+var numbers = '1234567890';
+
+var pwd = '';
+
+var lowerSelection = false;
+var upperSelection = false;
+var specialSelection = false;
+var numberSelection = false;
+
+
+//function to generate a random password
