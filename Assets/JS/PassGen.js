224ae37ea70e8c8e4549575f7120fcd7555b642d
Patrick Walker <pat31477@yahoo.com>
2021-02-25 11:28:52 -0500

    Added image folder and updating Readme

--- Assets/JS/PassGen.js
+++ Assets/JS/PassGen.js
@@ -1,4 +1,7 @@
 
+window.onload = alert("Welcome! Please click 'Generate Password' to start!");
+
+
 //define variables
 
 var lower = 'abcdefghijklmnopqrstuvwxyz';
@@ -15,3 +18,62 @@ var numberSelection = false;
 
 
 //function to generate a random password
+
+
+function generate() {
+    var confirmLength = '';
+//asking user to input desired character length
+    while (isNaN(confirmLength) || confirmLength < 8 || confirmLength > 128) {
+        confirmLength = prompt("What length would you like the password to be? (Between 8 to 128 characters)");
+        if (confirmLength === null) {
+            break;
+        }
+    }
+//getting user input for which character types to use for the password
+    if (confirmLength) {
+        if (confirm("Would you like to use lowercase characters?") == true) {
+            lowerSelection = true
+        } 
+
+        if (confirm("Would you like to use uppercase characters?") == true) {
+            upperSelection = true
+        }
+
+        if (confirm("Would you like to use special characters?") == true) {
+            specialSelection = true
+        }
+
+        if (confirm("Would you like to use numerical characters?") == true) {
+            numberSelection = true
+        }
+//if none of the character types are selected, alerts the user to choose at least one
+        if (lowerSelection === false && upperSelection === false && specialSelection === false && numberSelection === false) {
+            alert("At least one character type must be selected")
+        }
+    }
+//generate random password
+    var characters = '';
+    characters += (lowerSelection ? lower : '');
+    characters += (upperSelection ? upper : '');
+    characters += (specialSelection ? special : '');
+    characters += (numberSelection ? numbers : '');
+
+    pwd = password(confirmLength, characters);
+
+    document.getElementById("password").innerHTML = pwd;
+
+}
+
+function password(l, characters) {
+    var pwd = '';
+    for (var i = 0; i < l; ++i) {
+        pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return pwd;
+}
+//function to copy password to computer clipboard
+function copied() {
+    document.getElementById("password").select();
+    document.execCommand("copy");
+    alert("The password has been copied to your clipboard!");
+}
